f94e28aafb7eeda756101284c5410d291944236b
valery <valery.fremaux@gmail.com>
2018-05-02 15:10:18 +0200

    fix sortorder inversion + notices

--- xlib.php
+++ xlib.php
@@ -47,64 +47,56 @@ function check_userquiz_monitor_review_applicability($attemptobj) {
     }
 }
 
-/**
- * Adds Jquery form control for single question quizzes
- */
-function block_userquiz_monitor_attempt_adds($attemptobj) {
-    global $PAGE;
+function block_userquiz_monitor_attempt_buttons($attemptobj, $page) {
+    global $OUTPUT, $CFG;
 
     $course = $attemptobj->get_course();
 
-    $PAGE->requires->jquery();
     $config = block_userquiz_monitor_check_has_quiz($course, $attemptobj->get_quizid());
     if ($config) {
-        if (($config->mode == 'exam' && !empty($config->examforceanswer)) ||
-                ($config->mode == 'training' && !empty($config->trainingforceanswer))) {
-            $PAGE->requires->js_call_amd('block_userquiz_monitor/quizforceanswer', 'init');
+        $template = new StdClass;
+
+        if (is_dir($CFG->dirroot.'/mod/quiz/accessrule/usernumattempts')) {
+            $ruleinstance = quizaccess_usernumattempts::make($attemptobj->get_quizobj(), time(), true);
+            if ($ruleinstance && $ruleinstance->is_enabled()) {
+                $template->label = get_string('returntotraining', 'block_userquiz_monitor');
+            } else {
+                $template->label = get_string('returntoquiz', 'block_userquiz_monitor');
+            }
+        } else {
+            $template->label = get_string('returntoquiz', 'block_userquiz_monitor');
         }
-        if ($config->protectcopy) {
-            $PAGE->requires->js_call_amd('block_userquiz_monitor/quizprotectcopy', 'init');
+
+        // Get the url for finishing and registering attempt.
+        $params = array(
+            'attempt' => $attemptobj->get_attemptid(),
+            'finishattempt' => 1,
+            'timeup' => 0,
+            'slots' => '',
+            'sesskey' => sesskey(),
+        );
+
+        $template->finishurl = new moodle_url($attemptobj->processattempt_url(), $params);
+
+        $template->isnotfirstpage = false;
+        if ($page > 0) {
+            $navmethod = $attemptobj->get_quiz()->navmethod;
+            if ($navmethod == 'free') {
+                // This accessorily disables back nav.
+                $template->isnotfirstpage = true;
+            }
+            $template->navigatepreviousstr = get_string('navigateprevious', 'quiz');
+        }
+        if ($attemptobj->is_last_page($page)) {
+            $template->nextlabelstr = get_string('endtest', 'quiz');
+        } else {
+            $template->nextlabelstr = get_string('navigatenext', 'quiz');
         }
 
-        $PAGE->requires->js_call_amd('block_userquiz_monitor/quiztrapoutlinks', 'init');
-    }
-}
-
-/**
- * Adds Jquery form control for single question quizzes
- */
-function block_userquiz_monitor_protect_page($attemptobj) {
-    global $PAGE;
-
-    $course = $attemptobj->get_course();
-
-    $PAGE->requires->jquery();
-    $config = block_userquiz_monitor_check_has_quiz($course, $attemptobj->get_quizid());
-    if ($config) {
-        if ($config->protectcopy) {
-            $PAGE->requires->js_call_amd('block_userquiz_monitor/quizprotectcopy', 'init');
-        }
-    }
-}
-
-function block_userquiz_monitor_add_body_classes($attemptobj) {
-    global $PAGE;
-
-    $uqconfig = block_userquiz_monitor_check_has_quiz_ext($attemptobj->get_course(), $attemptobj->get_quizid());
-    if (empty($uqconfig)) {
-        return;
-    }
-    if (($uqconfig->mode == 'training' && $uqconfig->trainingforceanswer) ||
-            ($uqconfig->mode == 'exam' && $uqconfig->examforceanswer)) {
-        $PAGE->add_body_class('is-userquiz');
-        $PAGE->add_body_class('userquiz-'.$uqconfig->mode);
-    }
-    if (($uqconfig->mode == 'training' && $uqconfig->trainingnobackwards) ||
-            ($uqconfig->mode == 'exam' && $uqconfig->examnobackwards)) {
-        $PAGE->add_body_class('no-backwards');
+        return $OUTPUT->render_from_template('block_userquiz_monitor/attemptpagenavigation', $template);
     }
 
-    return $uqconfig;
+    return false;
 }
 
 function block_userquiz_monitor_check_has_quiz_ext($course, $quizid) {
