679884f6fd37a92c2866c986a0420e8fee174b9d
valery <valery.fremaux@gmail.com>
2018-11-19 13:44:49 +0100

    Fix details about available attempts, add control params for history and detail view

--- version.php
+++ version.php
@@ -33,4 +33,4 @@ $plugin->maturity = MATURITY_RC;
 $plugin->release = '3.1.0 (Build 2017121704)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0004';
+$plugin->codeincrement = '3.1.0006';
