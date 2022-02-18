3f743cc58b01f809fa0ce939a38103d482b65674
Valery Fremaux <valery.fremaux@gmail.com>
2022-02-18 15:25:49 +0100

    Automated sync from SVN

--- settings.php
+++ settings.php
@@ -29,7 +29,8 @@ if ($ADMIN->fulltree) {
     if (block_userquiz_monitor_supports_feature('emulate/community') == 'pro') {
         // This will accept any.
         include_once($CFG->dirroot.'/blocks/userquiz_monitor/pro/prolib.php');
-        \block_userquiz_monitor\pro_manager::add_settings($ADMIN, $settings);
+        $promanager = block_userquiz_monitor\pro_manager::instance();
+        $promanager->add_settings($ADMIN, $settings);
     } else {
         $label = get_string('plugindist', 'block_userquiz_monitor');
         $desc = get_string('plugindist_desc', 'block_userquiz_monitor');
